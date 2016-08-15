4c98c1da34a36c5667bdc2ae13ccbce18d82e1f8
vagrant <vagrant@precise32.(none)>
2016-08-15 16:06:44 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onlineeducator::Application.config.secret_key_base = '2bce9be2b3bb7e2b84b3fa1f59267290344541118c68d65d2059dc666814307a1faa32837a64354a2f107c5d94bd318a5092dff4fa32def50f87a9a383f9dfc3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Onlineeducator::Application.config.session_store :cookie_store, key: '_onlineeducator_session'
